8aa1c33eb556815222f22d3607f2c2ec7218ed3c
Sandra Arato <sandra@idoescode.com>
2024-05-08 10:24:01 +1000

    aded page styling

--- src/App.js
+++ src/App.js
@@ -38,7 +38,9 @@ Dedicated Customer Service: Our friendly support team is here to assist you with
       </section>
       <section><h2>Join the Tidy Toys Community Today!</h2>
         Simplify playtime, prioritize sustainability, and reclaim precious moments with your family. Subscribe to Tidy Toys now and experience the joy of hassle-free, eco-friendly playtime delivered straight to your doorstep.
-      </section>
+        </section>
+        
+        <button>Join our Beta Today!</button>
       </main>
     </div>
   );
