3ea41a008b99fcbc4c46c133fef6f1c896dad5f1
Sandra Arato <sandra@idoescode.com>
2024-06-09 10:13:39 +1000

    updated terms

--- src/components/TermsAndConditions/Terms.js
+++ src/components/TermsAndConditions/Terms.js
@@ -5,126 +5,67 @@ const Terms = () => {
   return (
     <main className={styles.terms}>
       <div>
-      <h2><strong>Terms and Conditions</strong></h2>
+        <h2><strong>Terms and Conditions</strong></h2>
 
-      <p>Welcome to tidytoys.co!</p>
+        <p>These terms and conditions ("Terms") govern your use of the Toy Rotation Rental Service provided by tidytoys.co. By accessing this website and using our services, you agree to be bound by these Terms. If you disagree with any part of these terms, please do not use our services.</p>
 
-      <p>These terms and conditions outline the rules and regulations for the use of tidytoys's Website, located at https://tidytoys.co.</p>
+        <h3>1. Service Description</h3>
 
-      <p>By accessing this website we assume you accept these terms and conditions. Do not continue to use tidytoys.co if you do not agree to take all of the terms and conditions stated on this page.</p>
+        <p>Toy Rotation Rental Service provides a toy rental service where customers can choose from weekly, fortnightly, and monthly toy boxes. These boxes are delivered to the customer's address and collected at the end of the rental term. Our aim is to provide a variety of high-quality toys that foster learning and play.</p>
 
-      <p>The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company's terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client's needs in respect of provision of the Company's stated services, in accordance with and subject to, prevailing law of au. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.</p>
+        <h3>2. Subscription and Fees</h3>
 
-      <h3><strong>Cookies</strong></h3>
+        <ul>
+          <li><strong>Subscription Plans</strong>: Customers can subscribe to weekly, fortnightly, or monthly plans. Each plan includes a curated selection of toys delivered to the customer's address.</li>
+          <li><strong>Fees</strong>: Subscription fees vary based on the chosen plan and are charged in advance. Fees are non-refundable except as specified in our cancellation policy.</li>
+          <li><strong>Payment</strong>: Payments are processed via [payment methods]. By subscribing, you authorize us to charge your selected payment method for recurring fees.</li>
+        </ul>
 
-      <p>We employ the use of cookies. By accessing tidytoys.co, you agreed to use cookies in agreement with the tidytoys's Privacy Policy. </p>
+        <h3>3. Delivery and Collection</h3>
 
-      <p>Most interactive websites use cookies to let us retrieve the user's details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.</p>
+        <ul>
+          <li><strong>Delivery</strong>: Toy boxes will be delivered to the customer's specified address at the beginning of each rental term.</li>
+          <li><strong>Collection</strong>: Toy boxes will be collected at the end of the rental term. Customers must ensure that all toys and packaging are returned in good condition.</li>
+          <li><strong>Missed Collections</strong>: If a collection is missed, the customer may incur additional fees.</li>
+        </ul>
 
-      <h3><strong>License</strong></h3>
+        <h3>4. Use and Care of Toys</h3>
 
-      <p>Unless otherwise stated, tidytoys and/or its licensors own the intellectual property rights for all material on tidytoys.co. All intellectual property rights are reserved. You may access this from tidytoys.co for your own personal use subjected to restrictions set in these terms and conditions.</p>
+        <ul>
+          <li><strong>Inspection</strong>: Parents/guardians are responsible for inspecting all toys upon delivery. Only toys deemed safe and suitable for the child’s age and ability should be given for independent play.</li>
+          <li><strong>Care</strong>: Toys should be used according to the manufacturer's guidelines. Do not allow children to misuse toys in a way that may cause damage or pose a safety risk.</li>
+        </ul>
 
-      <p>You must not:</p>
-      <ul>
-          <li>Republish material from tidytoys.co</li>
-          <li>Sell, rent or sub-license material from tidytoys.co</li>
-          <li>Reproduce, duplicate or copy material from tidytoys.co</li>
-          <li>Redistribute content from tidytoys.co</li>
-      </ul>
+        <h3>5. Damage</h3>
 
-      <p>This Agreement shall begin on the date hereof. Our Terms and Conditions were created with the help of the <a href="https://www.termsandconditionsgenerator.com/">Free Terms and Conditions Generator</a>.</p>
+        <ul>
+          <li><strong>Responsibility</strong>: Customers are responsible for any damage beyond normal wear and tear to the toys.</li>
+          <li><strong>Assessment and Charges</strong>: Fees for damaged toys will be assessed and charged based on each situation with the intent that the customer and company shall continue their agreement.</li>
+        </ul>
 
-      <p>Parts of this website offer an opportunity for users to post and exchange opinions and information in certain areas of the website. tidytoys does not filter, edit, publish or review Comments prior to their presence on the website. Comments do not reflect the views and opinions of tidytoys,its agents and/or affiliates. Comments reflect the views and opinions of the person who post their views and opinions. To the extent permitted by applicable laws, tidytoys shall not be liable for the Comments or for any liability, damages or expenses caused and/or suffered as a result of any use of and/or posting of and/or appearance of the Comments on this website.</p>
+        <h3>6. Loss</h3>
 
-      <p>tidytoys reserves the right to monitor all Comments and to remove any Comments which can be considered inappropriate, offensive or causes breach of these Terms and Conditions.</p>
+        <ul>
+          <li><strong>Responsibility</strong>: Customers are responsible for the loss of toys.</li>
+          <li><strong>Charges for Missing Toys</strong>: If a full box goes missing, the renter shall pay the replacement cost based on RRP prices of the toys included, and will be charged the full amount of the toys. Occasional missing toys are an acceptable risk as long as they are returned in a future box as soon as they are found. If up to 3 toys go missing during the loan period and are not returned in a reasonable time to the company, the company reserves the right to charge for the toys and cancel the agreement between the renter and the company.</li>
+        </ul>
 
-      <p>You warrant and represent that:</p>
+        <h3>7. Cancellation and Refund Policy</h3>
 
-      <ul>
-          <li>You are entitled to post the Comments on our website and have all necessary licenses and consents to do so;</li>
-          <li>The Comments do not invade any intellectual property right, including without limitation copyright, patent or trademark of any third party;</li>
-          <li>The Comments do not contain any defamatory, libelous, offensive, indecent or otherwise unlawful material which is an invasion of privacy</li>
-          <li>The Comments will not be used to solicit or promote business or custom or present commercial activities or unlawful activity.</li>
-      </ul>
+        <ul>
+          <li><strong>Cancellation</strong>: Customers can cancel their subscription at any time. Cancellations must be made before the next billing cycle to avoid charges for the upcoming term.</li>
+          <li><strong>Refunds</strong>: Refunds are only provided if toys are found to be defective upon delivery and reported within 48 hours.</li>
+        </ul>
 
-      <p>You hereby grant tidytoys a non-exclusive license to use, reproduce, edit and authorize others to use, reproduce and edit any of your Comments in any and all forms, formats or media.</p>
+        <h3>8. Liability and Safety</h3>
 
-      <h3><strong>Hyperlinking to our Content</strong></h3>
+        <ul>
+          <li><strong>Parental Responsibility</strong>: The safety of children using our toys is the responsibility of the parent/guardian. Parents/guardians must inspect each toy for safety and suitability before allowing independent play. Any toy that poses a potential risk should be removed from the child's access.</li>
+          <li><strong>Limitation of Liability</strong>: Toy Rotation Rental Service is not liable for any injuries, damages, or losses resulting from the use of rented toys. By accepting our terms, you acknowledge that the use of rented toys is at your own risk and discretion.</li>
+        </ul>
 
-      <p>The following organizations may link to our Website without prior written approval:</p>
-
-      <ul>
-          <li>Government agencies;</li>
-          <li>Search engines;</li>
-          <li>News organizations;</li>
-          <li>Online directory distributors may link to our Website in the same manner as they hyperlink to the Websites of other listed businesses; and</li>
-          <li>System wide Accredited Businesses except soliciting non-profit organizations, charity shopping malls, and charity fundraising groups which may not hyperlink to our Web site.</li>
-      </ul>
-
-      <p>These organizations may link to our home page, to publications or to other Website information so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products and/or services; and (c) fits within the context of the linking party's site.</p>
-
-      <p>We may consider and approve other link requests from the following types of organizations:</p>
-
-      <ul>
-          <li>commonly-known consumer and/or business information sources;</li>
-          <li>dot.com community sites;</li>
-          <li>associations or other groups representing charities;</li>
-          <li>online directory distributors;</li>
-          <li>internet portals;</li>
-          <li>accounting, law and consulting firms; and</li>
-          <li>educational institutions and trade associations.</li>
-      </ul>
-
-      <p>We will approve link requests from these organizations if we decide that: (a) the link would not make us look unfavorably to ourselves or to our accredited businesses; (b) the organization does not have any negative records with us; (c) the benefit to us from the visibility of the hyperlink compensates the absence of tidytoys; and (d) the link is in the context of general resource information.</p>
-
-      <p>These organizations may link to our home page so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products or services; and (c) fits within the context of the linking party's site.</p>
-
-      <p>If you are one of the organizations listed in paragraph 2 above and are interested in linking to our website, you must inform us by sending an e-mail to tidytoys. Please include your name, your organization name, contact information as well as the URL of your site, a list of any URLs from which you intend to link to our Website, and a list of the URLs on our site to which you would like to link. Wait 2-3 weeks for a response.</p>
-
-      <p>Approved organizations may hyperlink to our Website as follows:</p>
-
-      <ul>
-          <li>By use of our corporate name; or</li>
-          <li>By use of the uniform resource locator being linked to; or</li>
-          <li>By use of any other description of our Website being linked to that makes sense within the context and format of content on the linking party's site.</li>
-      </ul>
-
-      <p>No use of tidytoys's logo or other artwork will be allowed for linking absent a trademark license agreement.</p>
-
-      <h3><strong>iFrames</strong></h3>
-
-      <p>Without prior approval and written permission, you may not create frames around our Webpages that alter in any way the visual presentation or appearance of our Website.</p>
-
-      <h3><strong>Content Liability</strong></h3>
-
-      <p>We shall not be hold responsible for any content that appears on your Website. You agree to protect and defend us against all claims that is rising on your Website. No link(s) should appear on any Website that may be interpreted as libelous, obscene or criminal, or which infringes, otherwise violates, or advocates the infringement or other violation of, any third party rights.</p>
-
-      <h3><strong>Reservation of Rights</strong></h3>
-
-      <p>We reserve the right to request that you remove all links or any particular link to our Website. You approve to immediately remove all links to our Website upon request. We also reserve the right to amen these terms and conditions and it's linking policy at any time. By continuously linking to our Website, you agree to be bound to and follow these linking terms and conditions.</p>
-
-      <h3><strong>Removal of links from our website</strong></h3>
-
-      <p>If you find any link on our Website that is offensive for any reason, you are free to contact and inform us any moment. We will consider requests to remove links but we are not obligated to or so or to respond to you directly.</p>
-
-      <p>We do not ensure that the information on this website is correct, we do not warrant its completeness or accuracy; nor do we promise to ensure that the website remains available or that the material on the website is kept up to date.</p>
-
-      <h3><strong>Disclaimer</strong></h3>
-
-      <p>To the maximum extent permitted by applicable law, we exclude all representations, warranties and conditions relating to our website and the use of this website. Nothing in this disclaimer will:</p>
-
-      <ul>
-          <li>limit or exclude our or your liability for death or personal injury;</li>
-          <li>limit or exclude our or your liability for fraud or fraudulent misrepresentation;</li>
-          <li>limit any of our or your liabilities in any way that is not permitted under applicable law; or</li>
-          <li>exclude any of our or your liabilities that may not be excluded under applicable law.</li>
-      </ul>
-
-      <p>The limitations and prohibitions of liability set in this Section and elsewhere in this disclaimer: (a) are subject to the preceding paragraph; and (b) govern all liabilities arising under the disclaimer, including liabilities arising in contract, in tort and for breach of statutory duty.</p>
-
-      <p>As long as the website and the information and services on the website are provided free of charge, we will not be liable for any loss or damage of any nature.</p>
-    </div>
+        <p>By using our service, you agree to abide by these Terms and Conditions. We reserve the right to amend these terms at any time without prior notice. It is your responsibility to review these terms periodically for changes. If you continue to use our services after any modifications to these terms, it constitutes your acceptance of those changes.</p>
+      </div>
     </main>
   );
 };
